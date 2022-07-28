dfa0b5fbe164cda26815174b78c00ea350c762ad
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2022-07-28 09:44:02 +0100

    glade example

--- gtk_ch25_glade/main.c
+++ gtk_ch25_glade/main.c
@@ -10,3 +10,26 @@
 
 // For gtk3, you need glade 
 // For gtk2, you need glade-3, which is no longer in Ubuntu (arghhh)
+
+#include <gtk/gtk.h>
+
+void end_program(GtkWidget *wid, gpointer ptr);
+
+int main (int argc, char *argv[])
+{
+    gtk_init (&argc, &argv);
+    GtkBuilder *builder = gtk_builder_new ();
+    gtk_builder_add_from_file (builder, "mylayout.glade", NULL);
+    GtkWidget *win = (GtkWidget *) gtk_builder_get_object (builder,"window1");
+    GtkWidget *btn = (GtkWidget *) gtk_builder_get_object (builder,"button1");
+    g_signal_connect(btn, "clicked", G_CALLBACK(end_program), NULL);
+    
+    gtk_widget_show_all (win);
+    gtk_main ();
+    return 0;
+}
+
+void end_program(GtkWidget *wid, gpointer ptr)
+{
+	gtk_main_quit();
+}
